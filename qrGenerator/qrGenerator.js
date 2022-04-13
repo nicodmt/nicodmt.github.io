91c666c4c8188e2e4e6b4b240bf1e57837709656
Nicolas Dumont <nicolas.dumont@hotmail.se>
2022-04-13 16:20:58 +0200

    Fresh restart...

--- qrGenerator/qrGenerator.js
+++
@@ -1,7 +0,0 @@
-function getQR(){
-    var userInput = document.getElementById("input").value;
-    var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userInput}`;
-    var img = document.getElementById("img");
-
-    img.src = url;
-}
